9474a30a338ae9246dad2f66d3f78b5e587ce2bc
mail945 <mail945@mail.ru>
2017-04-17 21:43:41 +0300

    Add files via upload

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,9 +39,10 @@ end
 
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
-#task :setup do
-###   command %{rbenv install 2.4.1}
-#end
+task :setup do
+###   command %{rbenv install 2.4.1
+ comment "Be sure to edit '#{fetch(:shared_path)}/config/database.yml', 'secrets.yml' and puma.rb."
+end
 
 desc "Deploys the current version to the server."
 task :deploy do
